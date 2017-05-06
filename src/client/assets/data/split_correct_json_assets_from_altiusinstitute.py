05c56207f3a5137060b22e778c6ece1cac7ae989
Alex Reynolds <alexpreynolds@gmail.com>
2017-05-06 02:15:34 -0700

    Add chromHMM track and JSON object in single group JSON files

--- src/client/assets/data/split_correct_json_assets_from_altiusinstitute.py
+++ src/client/assets/data/split_correct_json_assets_from_altiusinstitute.py
@@ -23,10 +23,42 @@ j = json.loads(s)
 
 g = j[3]
 
+# chromHMM object
+
+c = { "height":225,
+      "url":"https://epilogos.altiusinstitute.org/assets/data/chromHMM/hg19_chromHMM_15.gz",
+      "mode":"show",
+      "type":"categoryMatrix",
+      "name":"15-state model (observed)",
+      "rowcount":127,
+      "rowheight":2,
+      "epg_order":0,
+      "assay_order":0,
+      "etc_order":0,
+      "metadata":{},
+      "categories":{
+          "1":["Active TSS","#ff0000"],
+          "2":["Flanking Active TSS","#ff4500"],
+          "3":["Transcr at gene 5\' and 3\'","#32cd32"],
+          "4":["Strong transcription","#008000"],
+          "5":["Weak transcription","#006400"],
+          "6":["Genic enhancers","#c2e105"],
+          "7":["Enhancers","#ffff00"],
+          "8":["ZNF genes & repeats","#66cdaa"],
+          "9":["Heterochromatin","#8a91d0"],
+          "10":["Bivalent/Poised TSS","#cd5c5c"],
+          "11":["Flanking Bivalent TSS/Enh","#e9967a"],
+          "12":["Bivalent Enhancer","#bdb76b"],
+          "13":["Repressed PolyComb","#808080"],
+          "14":["Weak Repressed PolyComb","#c0c0c0"],
+          "15":["Quiescent/Low","#ffffff"]
+      }
+  }
+
 # first URL in group pairing
 
 e = j[0]
-e['height'] = 450
+e['height'] = 225
 
 u = e['url']
 u_p = urlparse(u)
@@ -37,6 +69,7 @@ u_sample = '_'.join(u_comp[2:-1])
 new_fn = '_'.join(["qcat", u_pq_level, u_sample]) + ".json"
 new_j = []
 new_j.append(e)
+new_j.append(c)
 new_j.append(g)
 sys.stdout.write("Splitting out to: " + new_fn + "\n")
 with open(new_fn, 'w') as new_fh:
@@ -45,7 +78,7 @@ with open(new_fn, 'w') as new_fh:
 # second URL in group pairing
 
 e = j[1]
-e['height'] = 450
+e['height'] = 225
 
 u = e['url']
 u_p = urlparse(u)
@@ -56,6 +89,7 @@ u_sample = '_'.join(u_comp[2:-1])
 new_fn = '_'.join(["qcat", u_pq_level, u_sample]) + ".json"
 new_j = []
 new_j.append(e)
+new_j.append(c)
 new_j.append(g)
 sys.stdout.write("Splitting out to: " + new_fn + "\n")
 with open(new_fn, 'w') as new_fh:
