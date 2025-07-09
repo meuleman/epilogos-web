c698cd06efe898f48e5ee3368dcaff6b14a5a63a
Alex Reynolds <alexpreynolds@gmail.com>
2025-07-09 10:32:05 -0700

    9 Jul 2025 patches (log qt mode viewconf URL)

--- src/Helpers.js
+++ src/Helpers.js
@@ -1565,7 +1565,7 @@ export const simSearchQueryPromise = (qChr, qStart, qEnd, qWindowSizeKb, self, i
 //
 export const uuidQueryPromise = function(fn, self, endpointURL) {
   const hgUUIDQueryURL = (!endpointURL) ? `${Constants.viewerHgViewParameters.hgViewconfEndpointURL}/api/v1/tilesets?ac=${fn}` : `${endpointURL}/tilesets?ac=${fn}`;
-  // console.log(`hgUUIDQueryURL ${hgUUIDQueryURL}`);
+  console.log(`hgUUIDQueryURL ${hgUUIDQueryURL}`);
   return axios.get(hgUUIDQueryURL).then((res) => {
     if (res.data && res.data.results && res.data.results[0]) {
       return res.data.results[0].uuid;
